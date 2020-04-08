354b41fd8dc12f1ba8ef3a08ad6bfa830d020da5
Susumu Takahashi <stakahas@mail.doshisha.ac.jp>
2020-04-08 10:20:07 +0900

    spike sorting dir

---
+++ spikesorting/master_kilosort4win.m
@@ -0,0 +1,55 @@
+function master_kilosort4win(filename)
+
+if nargin==0
+    filename='test2.bin';
+end
+
+% default options are in parenthesis after the comment
+homefn='c:/users/stakahas/Documents/Matlab/';% change this directory first
+KSfn=fullfile(homefn,'externalPackages/KiloSort');
+addpath(genpath(KSfn)); % path to kilosort folder
+NPYfn=fullfile(homefn,'externalPackages/npy-matlab');
+addpath(genpath(NPYfn)); % path to npy-matlab scripts
+
+%pathToYourConfigFile = '/home/tsusumu/KiloSort/configFiles'; % take from Github folder and put it somewhere else (together with the master_file)
+pathToYourConfigFile = fullfile(homefn,'externalPackages/KiloSort/configFiles');
+
+%from StandardConfig_MOVEME.m
+cwfn=pwd;
+ops.fbinary             = fullfile(cwfn,filename); % will be created for 'openEphys'		
+ops.fproc               = fullfile(cwfn,'temp_wh.dat'); % residual from RAM of preprocessed data		
+ops.root                = cwfn;
+ops.chanMap             = fullfile(KSfn,'configFiles/tetrode64.mat'); % make this file using createChannelMapFile.m	
+
+run(fullfile(pathToYourConfigFile, 'StandardConfig_MOVEME.m'))
+
+tic; % start timer
+%
+if ops.GPU     
+    gpuDevice(1); % initialize GPU (will erase any existing GPU arrays)
+end
+
+if strcmp(ops.datatype , 'openEphys')
+   ops = convertOpenEphysToRawBInary(ops);  % convert data, only for OpenEphys
+end
+%
+[rez, DATA, uproj] = preprocessData(ops); % preprocess data and
+                                          % extract spikes for
+                                          % initialization
+
+rez                = fitTemplates(rez, DATA, uproj);  % fit templates iteratively
+rez                = fullMPMU(rez, DATA);% extract final spike times (overlapping extraction)
+
+% AutoMerge. rez2Phy will use for clusters the new 5th column of st3 if you run this)
+%     rez = merge_posthoc2(rez);
+
+% save matlab results file
+save(fullfile(ops.root,  'rez.mat'), 'rez', '-v7.3');
+
+% save python results file for Phy
+rezToPhy(rez, ops.root);
+
+% remove temporary file
+delete(ops.fproc);
+%% 
+%
