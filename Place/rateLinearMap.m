c03a11b52f4eba0d98342f4628b275ac8338e2a9
tsusumu <stakahas@mail.doshisha.ac.jp>
2021-02-24 12:15:58 +0900

    ratemapSmooth

--- Place/rateLinearMap.m
+++ Place/rateLinearMap.m
@@ -1,27 +1,31 @@
 function [rate_map, xdim, occupancy, no_occupancy] = rateLinearMap(spk_x,x,spatial_scale,fs_video,fieldShuffleON)
 
 binside=2.5;
+GRID=[9 9];
+
 std_smooth_kernel=5;
 %[occupancy, xdim, ydim] = Occupancy(x,y,
 %spatial_scale,binside,fs_video);
 dim=binside;;
 
+min(x)
+max(x)
 xdim = min(x):spatial_scale^-1*dim:max(x); %edges of x and y
                                            %dimensions
 occupancy = histcounts(x, xdim)/fs_video;
-occupancy = OmitIslands(occupancy);
-
 no_occupancy = occupancy==0; 
 %spikes = hist3([spk_x, spk_y], 'Edges', {xdim, ydim});
 spikes = histcounts(spk_x, xdim);
-
-rate_map = SmoothMat(spikes, 10, 2)./SmoothMat(occupancy, 10, 2); 
-
+rate_map=spikes./occupancy;
 rate_map(no_occupancy) = 0; % set no occupancy to zero
+rate_map=SmoothMat(rate_map,GRID,binside);
+%rate_map = SmoothMat(spikes, 10, 2)./SmoothMat(occupancy, 10, 2); 
+
 rate_map = rate_map'; % returns these three
 occupancy = occupancy';
 no_occupancy = no_occupancy';
-    
+
+
 return;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%5555555555
--- Place/ratemap.m
+++ Place/ratemap.m
@@ -1,16 +1,19 @@
 function [rate_map, xdim, ydim, occupancy, no_occupancy] = ratemap(spk_x,spk_y,x,y,spatial_scale,fs_video,fieldShuffleON)
 
 binside=2.5;
-std_smooth_kernel=5;
+
+GRID=[9 9];
 [occupancy, xdim, ydim] = Occupancy(x,y, spatial_scale,binside,fs_video);
 occupancy = OmitIslands(occupancy);
 
 no_occupancy = occupancy==0; 
 spikes = hist3([spk_x, spk_y], 'Edges', {xdim, ydim});
 
-rate_map = SmoothMat(spikes, [5*std_smooth_kernel/binside, 5*std_smooth_kernel/binside], std_smooth_kernel/binside)./SmoothMat(occupancy, [5*std_smooth_kernel/binside, 5*std_smooth_kernel/binside], std_smooth_kernel/binside); 
+%rate_map = SmoothMat(spikes, [5*std_smooth_kernel/binside, 5*std_smooth_kernel/binside], std_smooth_kernel/binside)./SmoothMat(occupancy, [5*std_smooth_kernel/binside, 5*std_smooth_kernel/binside], std_smooth_kernel/binside); 
 
+rate_map=spikes./occupancy;
 rate_map(no_occupancy) = 0; % set no occupancy to zero
+rate_map=SmoothMat(rate_map, GRID,binside);
 rate_map = rate_map'; % returns these three
 occupancy = occupancy';
 no_occupancy = no_occupancy';
