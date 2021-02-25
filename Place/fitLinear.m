1fe2cef4b3948604bcecb8597715c13fad6bcfc6
tsusumu <stakahas@mail.doshisha.ac.jp>
2021-02-25 16:29:48 +0900

    8points

--- Place/fitLinear.m
+++ Place/fitLinear.m
@@ -1,30 +1,44 @@
-function [LTraj,GI]=fitLinear(Traj)
+function [LTraj,GI]=fitLinear2(Traj)
 h=figure;
 plot(Traj(:,3),Traj(:,4));
 fprintf(['Check the four corners of the rectangule with the mouse\n' ...
          'in a clockwise direction\n']);
 fprintf('Please start at the upper left corner');
-[x,y]=ginput(4)
-
+[x,y]=ginput(8);
 
 x1=[x(1):x(2)];
-y1=[ones(1,length(x1))*y(1)];
+y1=[y(1):((y(2)-y(1))./(length(x1)-1)):y(2)];
 
-y2=[y(1):-1:y(3)];
-x2=[ones(1,length(y2))*x(2)];
+x2=[x(2):x(3)];
+y2=[ones(1,length(x2))*y(2)];
 
-x3=[x(2):-1:x(4)];
-y3=[ones(1,length(x3))*y(3)];
+x3=[x(3):x(4)];
+y3=[y(2):((y(4)-y(2))./(length(x3)-1)):y(4)];
 
-y4=[y(3):y(1)];
-x4=[ones(1,length(y4))*x(1)];
-GI(1,:)=[x1 x2 x3 x4];
-GI(2,:)=[y1 y2 y3 y4];
+y4=[y(4):-1:y(5)];
+x4=[ones(1,length(y4))*x(4)];
+
+x5=[x(4):-1:x(6)];
+y5=[y(5):((y(6)-y(5))./(length(x5)-1)):y(6)];
+
+x6=[x(6):-1:x(7)];
+y6=[ones(1,length(x6))*y(6)];
+
+x7=[x(7):-1:x(8)];
+y7=[y(6):((y(8)-y(6))./(length(x7)-1)):y(8)];
+
+y8=[y(8):y(1)];
+x8=[ones(1,length(y8))*x(1)];
+
+GI(1,:)=[x1 x2 x3 x4 x5 x6 x7 x8];
+GI(2,:)=[y1 y2 y3 y4 y5 y6 y7 y8];
+
 
 hold on;
 plot(GI(1,:),GI(2,:),'r');
 
 LTraj=dsearchn(GI',Traj(:,3:4));
 
+pause(5);
 close(h);
 return;
