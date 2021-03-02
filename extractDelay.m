9cab3f1f6a155714f1d0edc0d4a3d4c2c0639ffd
tsusumu <stakahas@mail.doshisha.ac.jp>
2021-03-02 14:16:10 +0900

    bug fix

--- extractDelay.m
+++ extractDelay.m
@@ -2,18 +2,23 @@
 %load event.mat
 %plotRaster(kkOut{4,3},event,[1 2],'binWidth',100);
 %%%
-function spks=extractDelay(spks,event,nums,varargin)
+function [spks,Up,Do]=extractDelay(spks,event,nums,varargin)
 p = inputParser;
 p.addParamValue('binwidth', 100, @isnumeric);
 p.addParamValue('samplingrate', 25, @isnumeric);
-p.addParamValue('jitter', 0, @isnumeric);
+p.addParamValue('jitterpre', 0, @isnumeric);
+p.addParamValue('jitterpost', 0, @isnumeric);
 p.addParamValue('smooth', 0, @isnumeric);
 
 p.parse(varargin{:});
 binWidth = p.Results.binwidth;
 kHz=p.Results.samplingrate;
-jitter=p.Results.jitter;
+jitterPre=p.Results.jitterpre;
+jitterPost=p.Results.jitterpost;
 sm = p.Results.smooth;
+
+jitterPre=jitterPre*1000*kHz;
+jitterPost=jitterPost*1000*kHz;
 
 spks=double(spks);
 orgSpks=spks;
@@ -41,15 +46,15 @@ for m=1:loop
        %Up=Up(1:l);
        %Do=Do(1:l);
        Do=[ Do realmax];
+    else
+        l=length(Up);
     end
     
     eventNum=l;
-    jitter=jitter*1000*kHz;
-
     for i=l:-1:1
-         delInd=[delInd find(orgSpks>=Up(i)-jitter & orgSpks<=Do(i)+jitter)];
+         delInd=[delInd find(orgSpks>=Up(i)-jitterPre & orgSpks<=Do(i)+jitterPost)];
     end
-
+    
 end
 
 spks(delInd)=[];
