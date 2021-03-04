dfad78f5e63e34cf41712b985e367bf04aa1f503
tsusumu <stakahas@mail.doshisha.ac.jp>
2021-03-04 17:23:50 +0900

    speed option

--- Place/SequencePmap.m
+++ Place/SequencePmap.m
@@ -1,5 +1,9 @@
-function [seq,order,oc_map,LTraj,LTrajS]=SequencePmap(ensemble,event,nums,Traj,PosT,LTraj)
+function [seq,order,oc_map,LTraj,LTrajS]=SequencePmap(ensemble,event,nums,Traj,PosT,ThS,LTraj)
 if nargin==5
+    ThS=2.5;
+end
+
+if nargin<=6
     [LTraj,LTrajS]=fitLinear(Traj);
 end
 
@@ -7,7 +11,7 @@ loop=size(ensemble,1);
 for i=1:loop
     fprintf('%d/%d\n',i,loop);
     [seq(i,:),~,~,oc_map]=pmap(extractDelay(ensemble{i,3},event,nums),LTraj,PosT,0,'animal','rat', ...
-                 'verbose',0);
+                 'speed',ThS,'verbose',0);
 end
 
 [maxSeq,ind]=max(seq,[],2);
