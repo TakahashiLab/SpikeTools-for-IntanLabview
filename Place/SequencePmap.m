fc2b5ac20a38c36c3b2b85552285f390ae58d1db
tsusumu <stakahas@mail.doshisha.ac.jp>
2021-03-11 13:27:59 +0900

    outputMode

--- Place/SequencePmap.m
+++ Place/SequencePmap.m
@@ -1,12 +1,19 @@
-function [seq,order,oc_map,LTraj,LTrajS,TMpoints]=SequencePmap(ensemble,event,nums,Traj,PosT,ThS,LTraj)
+function [seq,order,oc_map,LTraj,LTrajS,TMpoints]=SequencePmap(ensemble,event,nums,Traj,PosT,ThS,LTraj,varargin)
 if nargin==5
     ThS=2.5;
 elseif nargin<=6
     [LTraj,LTrajS,TMpoints]=fitLinear(Traj);
-elseif nargin==7
+elseif nargin>=7
     LTrajS=[];
     TMpoints=[];
 end
+
+p = inputParser;
+p.addParamValue('outputmode', 'normalized', @ischar);
+
+p.parse(varargin{:});
+outputMode = p.Results.outputmode;
+
 
 loop=size(ensemble,1);
 for i=1:loop
@@ -17,7 +24,13 @@ end
 
 [maxSeq,ind]=max(seq,[],2);
 [~,ind]=sort(ind);
-seq=seq./maxSeq;
+
+switch (outputMode)
+  case 'normalized',
+    seq=seq./maxSeq;
+  case 'raw',
+end
+
 order=ind;
 
 return;
