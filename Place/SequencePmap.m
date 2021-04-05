eb072e2a1b6eb0af05619c353e32d0cf2757e57c
tsusumu <stakahas@mail.doshisha.ac.jp>
2021-04-05 16:22:30 +0900

    add options

--- Place/SequencePmap.m
+++ Place/SequencePmap.m
@@ -1,18 +1,26 @@
-function [seq,order,oc_map,LTraj,LTrajS,TMpoints]=SequencePmap(ensemble,event,nums,Traj,PosT,ThS,LTraj,varargin)
-if nargin==5
-    ThS=2.5;
-elseif nargin<=6
-    [LTraj,LTrajS,TMpoints]=fitLinear(Traj);
-elseif nargin>=7
-    LTrajS=[];
-    TMpoints=[];
-end
+function [seq,order,oc_map,LTraj,LTrajS,TMpoints]=SequencePmap(ensemble,event,nums,Traj,PosT,ThS,varargin)
 
 p = inputParser;
-p.addParamValue('outputmode', 'normalized', @ischar);
-
+p.addParamValue('outputmode', 'raw', @ischar);
+p.addParamValue('ltraj', [], @ismatrix);
+p.addParamValue('jitterpre', 0, @isnumeric);
+p.addParamValue('jitterpost', 0, @isnumeric);
 p.parse(varargin{:});
 outputMode = p.Results.outputmode;
+LTraj = p.Results.ltraj;
+jitterPre=p.Results.jitterpre;
+jitterPost=p.Results.jitterpost;
+
+if nargin<=5
+    ThS=2.5;
+    if isempty(LTraj)
+        [LTraj,LTrajS,TMpoints]=fitLinear(Traj);
+    end
+elseif nargin<=6
+    if isempty(LTraj)
+        [LTraj,LTrajS,TMpoints]=fitLinear(Traj);
+    end
+end
 
 
 loop=size(ensemble,1);
@@ -21,7 +29,7 @@ for i=1:loop
     if isempty(nums)
         [seq(i,:),~,~,oc_map]=pmap(ensemble{i,3},LTraj,PosT,0,'animal','rat','speed',ThS,'verbose',0);
     else
-        [seq(i,:),~,~,oc_map]=pmap(extractDelay(ensemble{i,3},event,nums),LTraj,PosT,0,'animal','rat', ...
+        [seq(i,:),~,~,oc_map]=pmap(extractDelay(ensemble{i,3},event,nums,'jitterPre',jitterPre,'jitterPost',jitterPost),LTraj,PosT,0,'animal','rat', ...
                  'speed',ThS,'verbose',0);
     end
 end
