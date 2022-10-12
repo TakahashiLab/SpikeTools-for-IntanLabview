5659d813da8076efb572f0505920c5ce352a22cf
tsusumu <stakahas@mail.doshisha.ac.jp>
2022-10-12 18:43:40 +0900

    init

---
+++ spikesorting/savePhy.m
@@ -0,0 +1,13 @@
+function ensemble=savePhy()
+
+[cids,cgs]=readClusterGroupsCSV('cluster_group.tsv');
+goodUnits=cids((cgs==2));%good units
+clusters=readNPY('spike_clusters.npy');
+ts=readNPY('spike_times.npy');
+
+ensemble=cell(length(goodUnits),3);
+for i=1:length(goodUnits)
+	ensemble{i,3}=ts(clusters==goodUnits(i));
+end
+
+return
