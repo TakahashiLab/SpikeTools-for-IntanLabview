d2af27a8894a119a099114f1df786b33955945bf
tsusumu <stakahas@mail.doshisha.ac.jp>
2023-02-21 08:58:20 +0900

    update

--- PD/plotGainMap.m
+++ PD/plotGainMap.m
@@ -5,30 +5,41 @@ function outMph = plotGainMap(phaseHist, varargin)
     p.addParamValue('band', 'all', @ischar);
     p.addParamValue('hist', 'off', @ischar);
     p.addParamValue('topfreq', 120, @isnumeric);
-
+    p.addParamValue('control', cell(1, 1), @iscell);
+    p.addParamValue('xyaxis', 'off', @ischar);
     p.parse(varargin{:});
 
     disp = p.Results.display;
     band = p.Results.band;
     histon = p.Results.hist;
     topFreq = p.Results.topfreq;
+    C = p.Results.control;
+    phaseHistCtrl = C{1};
+    xyaxis = p.Results.xyaxis;
 
     sd = 2; %5
     edges = [-3 * sd:1:3 * sd];
     kernel = normpdf(edges, 0, sd) * sd;
     kernel = kernel ./ sum(kernel(:));
 
+    outMph = [];
     %imagesc(conv2(kernel,kernel,mean(phaseHist(:,[1:40 80:120],:),3)','same'));
     %mph=conv2(kernel,kernel,mean(phaseHist(:,[1:40 81:120],:),3)','same');
 
     switch band
         case 'theta',
-            frange = 4:11;
+            frange = 4:12;
         case 'beta',
             frange = 12:30;
         case 'gamma',
-            frange = 31:80;
-            otherwise,
+            frange = 31:120-2;
+        case 'slowgamma',
+            frange = 31:60;
+        case 'fastgamma',
+            frange = 60:80;
+        case 'all',
+            frange = 2:topFreq-2;
+        otherwise,
             frange = [];
     end
 
@@ -37,43 +48,45 @@ function outMph = plotGainMap(phaseHist, varargin)
     topFreq = topFreq / 2;
 
     switch lower(disp)
-        case 'both',
+        case 'div',
 
-            %mph=conv2(kernel,kernel,mean(phaseHist(:,[1:40],:),3)','same');
-            %mph=SmoothMat(mean(phaseHist(:,[1:40],:),3)', [9 9],2);
-            %mph=SmoothMat(mean(phaseHist(:,[1:topFreq],:),3)', [9 9],2);
             mph = mean(phaseHist(:, [1:topFreq], :), 3)';
-            mph = [mph(:, size(mph, 2) / 2 + 1:end) mph mph(:, 1:size(mph, 2) / 2)];
-            mph = SmoothMat(mph, [9 9], 2);
+            mphC = mean(phaseHistCtrl(:, [1:topFreq], :), 3)';
+            nC = nanmean(mphC, 2);
+            mph = mph ./ nC;
+            mph(nC == 0, :) = 0;
+
+            mph = repeatMph(mph, 0, [3 3], 1);
+            endMph = size(mph, 2);
+
             subplot(1, 2, 1);
-            %imagesc([mph(:,11:20) mph mph(:,1:10)]);
             imagesc(mph);
             title('ascending');
             set(gca, 'ydir', 'normal');
             set(gca, 'ytick', [1:5:topFreq], 'yticklabel', [0:5:topFreq] * 2);
             set(gca, 'xtick', [1 10 20 30 40], 'xticklabel', {'-2\pi', '-\pi', '0', '\pi', '2\pi'});
             hold on;
-            t = 0:40;
+            t = 0:endMph - 1;
             plot(t + 1, cos(t / pi) * topFreq / 2 + topFreq / 2, 'w');
-            plot([0 40], [4/2 4/2], 'k');
-            plot([0 40], [12/2 12/2], 'k');
-            plot([0 40], [30/2 30/2], 'k');
-            plot([0 40], [80/2 80/2], 'k');
-            plot([21 21], [0 topFreq], 'k');
+            plot([1 endMph], [4/2 4/2], 'k');
+            plot([1 endMph], [12/2 12/2], 'k');
+            plot([1 endMph], [30/2 30/2], 'k');
+            plot([1 endMph], [80/2 80/2], 'k');
+            plot([endMph / 2 endMph / 2], [0 topFreq], 'k');
 
             colorbar;
             xlabel('Phase [radian]');
             ylabel('Frequency [Hz]');
 
             subplot(1, 2, 2);
-            %%%mph=conv2(kernel,kernel,mean(phaseHist(:,[81:120],:),3)','same');
-            %mph=conv2(kernel,kernel,mean(phaseHist(:,[120:-1:81],:),3)','same');
-            %mph=SmoothMat(mean(phaseHist(:,[120:-1:81],:),3)', [9 9],2);
-            %mph=SmoothMat(mean(phaseHist(:,[120:-1:120-topFreq+1],:),3)', [9 9],2);
             mph = mean(phaseHist(:, [120:-1:120 - topFreq + 1], :), 3)';
-            mph = [mph(:, size(mph, 2) / 2 + 1:end) mph mph(:, 1:size(mph, 2) / 2)];
-            mph = SmoothMat(mph, [9 9], 2);
-            %    imagesc([mph(:,11:20) mph mph(:,1:10)]);
+            mphC = mean(phaseHistCtrl(:, [120:-1:120 - topFreq + 1], :), 3)';
+
+            nC = nanmean(mphC, 2);
+            mph = mph ./ nC;
+            mph(nC == 0, :) = 0;
+            mph = repeatMph(mph, 0, [3 3], 1);
+
             imagesc(mph);
             title('descending');
             set(gca, 'ydir', 'normal');
@@ -81,16 +94,15 @@ function outMph = plotGainMap(phaseHist, varargin)
             set(gca, 'ytick', [1:5:topFreq], 'yticklabel', [0:5:topFreq] * 2);
             set(gca, 'xtick', [1 10 20 30 40], 'xticklabel', {'-2\pi', '-\pi', '0', '\pi', '2\pi'});
             hold on;
-            t = 0:40;
-            plot(t + 1, cos(t / pi) * topFreq / 2 + topFreq / 2, 'w');
+
             xlabel('Phase [radian]');
             ylabel('Frequency [Hz]');
-
-            plot([0 40], [4/2 4/2], 'k');
-            plot([0 40], [12/2 12/2], 'k');
-            plot([0 40], [30/2 30/2], 'k');
-            plot([0 40], [80/2 80/2], 'k');
-            plot([21 21], [0 topFreq], 'k');
+            plot(t + 1, cos(t / pi) * topFreq / 2 + topFreq / 2, 'w');
+            plot([1 endMph], [4/2 4/2], 'k');
+            plot([1 endMph], [12/2 12/2], 'k');
+            plot([1 endMph], [30/2 30/2], 'k');
+            plot([1 endMph], [80/2 80/2], 'k');
+            plot([endMph / 2 endMph / 2], [0 topFreq], 'k');
 
             colormap(jet);
             colorbar;
@@ -99,14 +111,28 @@ function outMph = plotGainMap(phaseHist, varargin)
         case 'ascend',
             xr = [1:topFreq];
             xrange = [0:5:topFreq];
-            outMph = plotfnc(phaseHist, xr, xrange, kernel, frange, band, histon);
+
+            outMph = plotfnc(phaseHist, phaseHistCtrl, xr, xrange, kernel, frange, band, xyaxis, histon, topFreq);
 
         case 'descend',
-            xr = [120:-1:120 - topFreq + 1]; %81
+           
+            xr = [1:topFreq];
+            phaseHist=phaseHist(:,120:-1:61,:);
+            phaseHistCtrl=phaseHistCtrl(:,120:-1:61,:);
             xrange = [0:5:topFreq];
 
-            outMph = plotfnc(phaseHist, xr, xrange, kernel, frange, band, histon);
+            outMph = plotfnc(phaseHist, phaseHistCtrl, xr, xrange, kernel, frange, band, xyaxis, histon, topFreq);
             %%title('descending');
+
+        case 'both',
+        
+            xr = [1:topFreq];
+            xrange = [0:5:topFreq];
+      
+            phaseHist=(phaseHist(:,1:60,:)+phaseHist(:,120:-1:61,:))/2;
+            phaseHistCtrl=(phaseHistCtrl(:,1:60,:)+phaseHistCtrl(:,120:-1:61,:))/2;
+
+            outMph = plotfnc(phaseHist, phaseHistCtrl, xr, xrange, kernel, frange, band, xyaxis, histon, topFreq);
     end
 
     return;
@@ -115,135 +141,211 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-function outMph = plotfnc(phaseHist, xr, xrange, kernel, frange, band, histon)
-    binsize = 2;
-    %topFreq = 80;
-    %topFreq = topFreq / 2;
-    %xr = [1:topFreq];
-    %xrange = [0:5:topFreq];
+function mphV = plotfnc(phaseHist, phaseHistCtrl, xr, xrange, kernel, frange, band, xyaxis, histon, topFreq)
+    binsize = 1;
+
     outMph = [];
+    
+    %set frequency band
+    frange=[frange frange(end)+2];%offset 
+    frange = unique(round(frange / 2));
+   
+    xr = xr(frange);
+    
+    %normalization
+  
+    nC=nanmean(squeeze(nanmean(phaseHistCtrl,1)),1);
+   
+    nC(nC==0)=NaN;
+  
+    nCs=zeros(size(phaseHistCtrl));
+    nC=repmat(nC,size(nCs,2),1);
+   
+    for q=1:size(nCs,1)
+        nCs(q,:,:)=nC;
+    end 
+  
+    phaseHistN=phaseHist./nCs;
+    phaseHistCtrlN=phaseHistCtrl./nCs;
+   
 
-    if strcmp(band, 'all')
-        mph = mean(phaseHist(:, xr, :), 3)';
-        mph = [mph(:, size(mph, 2) / 2 + 1:end) mph mph(:, 1:size(mph, 2) / 2)];
+     if strcmp(xyaxis, 'neuronphase') %neuron x phase
+        xr=xr(1:end-1);
+        mph = squeeze(nanmean(phaseHist(:, xr, :), 2))';
+        mphC = squeeze(nanmean(phaseHistCtrlN(:, xr, :), 2))';
+        mphN = squeeze(nanmean(phaseHistN(:, xr, :), 2))';
+        mphStat = mphN;%statistical test
+     
+        mphV=mphN;%for bar  
+        
+        [maxMph, maxMphId] = max(mphV, [], 2);
+        [~, id] = sort(maxMph, 'descend');
+        %mph = mphV(id, :);
+        
+        mph = repeatMph(mph,0);
+        mph = log10(mph);
+        imagesc(mph);
+        set(gca, 'ytick', [1 size(mph, 1)], 'yticklabel', [1 size(mph, 1)]);
+        clim([-1 2]);
+        ylabel('Neuron number');
 
-    elseif strcmp(band, 'phase')
-        mph = squeeze(mean(phaseHist(:, xr, :), 2));
-        outMph = mph;
-        seMph = (std(mph, 0, 2) ./ sqrt(size(mph, 2))')'; %sem
-        mph = mean(mph, 2)';
+    elseif strcmp(xyaxis, 'freqphase') %freq x phase
+    
+        mph = nanmean(phaseHist(:, xr, :), 3)';
+        mphC = nanmean(phaseHistCtrlN(:, xr, :), 3)';
+        mphN = nanmean(phaseHistN(:, xr, :), 3)';
+  
 
-        %mph = mph(1:end - 1);
-        %seMph = seMph(1:end - 1);
-        %outMph = outMph(1:end - 1, :);
-
-        hf = length(mph) / 2;
-        mph = [mph(:, hf + 1:end) mph mph(:, 1:hf)];
-        seMph = [seMph(:, hf + 1:end) seMph seMph(:, 1:hf)];
-        [~, ~, bins] = histcounts(1:length(mph), length(mph) / binsize);
-        mph = accumarray(bins', mph, [], @mean)';
-        seMph = accumarray(bins', seMph, [], @mean)';
-
-        [~, ~, bins] = histcounts(1:size(outMph, 1), size(outMph, 1) / binsize);
-
-        binOutMph = zeros(size(outMph));
-        binOutMph(size(binOutMph, 1) / binsize + 1:end, :) = [];
-
-        for q = 1:size(outMph, 2)
-            binOutMph(:, q) = accumarray(bins', outMph(:, q), [], @mean)';
+        mphStat = mphN;%statistical test
+        
+        mphV=mphN;%for bar   
+        
+        mph = repeatMph(mph, 1, [3 3], 1);
+       %mph=repeatMph(mph,0);
+        
+        imagesc(mph);
+    
+        lenMph=size(mph,1);
+        if lenMph > 20
+            ytick = 1:floor(lenMph / 10):lenMph;
+        else
+            ytick = 1:lenMph;
         end
-
-        outMph = binOutMph;
-
+       
+        yticklabel = xr(ytick) * 2;
+        set(gca, 'ytick', ytick, 'yticklabel', yticklabel);
+        ylabel('Frequency [Hz]');
+    elseif strcmp(xyaxis, 'gainphase')
+        xr=xr(1:end-1);
+        mph = squeeze(nanmean(phaseHist(:, xr, :), 2))';
+        mphC = squeeze(nanmean(phaseHistCtrlN(:, xr, :), 2))';
+        mphN = squeeze(nanmean(phaseHistN(:, xr, :), 2))';
+        mphStat = mphN;%statistical test
+       
+        mphV=mphN;%for bar  
+        outMph = mph;
+        
+        seMph = (std(mph,0,1) ./ sqrt(size(mph,1))); %sem
+        mph = nanmean(mph, 1);
+        
+        mph = repeatMph(mph,0);
+        seMph = repeatMph(seMph,0);
+        errP=mph+seMph;
+        errN=mph-seMph;
+        mph=log10(mph);
+        errP=log10(errP);
+        errN=log10(errN);
+        
         plot(mph);
 
-        hold on; plot(mph + seMph, 'r'); plot(mph - seMph, 'r');
+        hold on; plot(errP, 'r'); plot(errN, 'r');
+        endMph = size(mph, 2);
+        t = 1:endMph;
+        set(gca, 'xtick', [1 endMph / 4 endMph / 4 * 2 endMph / 4 * 3 endMph], 'xticklabel', {'-2\pi', '-\pi', '0', '\pi', '2\pi'});
 
-        set(gca, 'xtick', [0:10 / binsize:40 / binsize], 'xticklabel', {'-2\pi', '-\pi', '0', '\pi', '2\pi'});
         xlabel('phase');
         ylabel('Gain');
 
-    elseif strcmp(band, 'freq')
-
+    elseif strcmp(xyaxis, 'gainfreq')
+        xr=xr(1:end-1);
         mph = squeeze(mean(phaseHist(:, xr, :), 1));
+        mphC = squeeze(mean(phaseHistCtrlN(:, xr, :), 1));
+        mphN = squeeze(mean(phaseHistN(:, xr, :), 1));
+        mphStat = mphN;%statistical test
+        %nC = nanmean(mphC, 1);%1:neuron,2:freq
+        %mph = mph ./ nC;
+        mphV=mphN;%for bar  
         outMph = mph;
+        mph=mph(2:end-1,:);
+
         mph = mean(mph, 2)';
-        [~, ~, bins] = histcounts(1:length(mph), length(mph) / binsize);
-        mph = accumarray(bins', mph, [], @mean)';
-        %mph=SmoothMat(mph,[3 3],1);
+        mph=log10(mph);
+
         plot(mph);
 
         set(gca, 'xtick', xrange, 'xticklabel', xrange * 2 * binsize);
         xlabel('Frequency [Hz]');
         ylabel('Gain');
-    else %other
-        frange = unique(floor(frange / 2));
 
-        mph = squeeze(mean(phaseHist(:, xr(frange), :), 2))';
-
-        [maxMph, maxMphId] = max(mph, [], 2);
-        [~, id] = sort(maxMphId, 'descend');
-        %mph = mph(:, id);
-
-        % [~, id] = sort(maxMph, 'descend');
-        mph = mph(id, :);
-       
-        mph = [mph(:, size(mph, 2) / 2 + 1:end) mph mph(:, 1:size(mph, 2) / 2)];
     end
 
-    if (strcmp(band, 'freq') | strcmp(band, 'phase'))
+    if (strcmp(xyaxis, 'gainfreq') | strcmp(xyaxis, 'gainphase'))
 
     elseif strcmp(histon, 'off')
 
-        mph = SmoothMat(mph, [9 9], 2);
-        %mph=conv2(kernel,kernel,mph,'same');
-        imagesc(mph);
-
         %%title('ascending');
         set(gca, 'ydir', 'normal');
-
-        if strcmp(band, 'all')
-            set(gca, 'ytick', xrange, 'yticklabel', xrange * 2);
-            ylabel('Frequency [Hz]');
-        else
-
-            if size(mph, 1) > 1
-                set(gca, 'ytick', [1 size(mph, 1)], 'yticklabel', [1 size(mph, 1)]);
-            end
-
-            ylabel('Neuron number');
-        end
-
-        set(gca, 'xtick', [1 10 20 30 40], 'xticklabel', {'-2\pi', '-\pi', '0', '\pi', '2\pi'});
+        mphY = size(mph, 1);
+        endMph = size(mph, 2);
+        t = 1:endMph;
+        set(gca, 'xtick', [1 endMph / 4 endMph / 4 * 2 endMph / 4 * 3 endMph], 'xticklabel', {'-2\pi', '-\pi', '0', '\pi', '2\pi'});
         hold on;
-        t = 0:40;
-        plot(t + 1, cos(t / pi) * 10 + 10.5, 'w');
-        xlabel('Phase [radian]');
+        plot(t, cos(t / max(t) * 4 * pi) * mphY / 2 + mphY / 2, 'w');
+        xlabel('Phase [ radian]');
         colormap(jet);
         colorbar;
 
-        %if size(mph, 1) ~= 1
         if ~isempty(mph)
 
             if ~isnan(mph(1))
-                caxis([0.5 max(mph(:))]);
+                %  caxis([0.5 max(mph(:))]);
             end
 
         end
 
-    else
-        mph2 = [];
-        bin = 1;
-        sd = 1;
-        edges = [-3 * sd:3 * sd];
-        kernel = normpdf(edges, 0, sd) * sd;
-        %mph=conv(mean(mph),kernel,'same');
-        mph = mean(mph);
-        mph = SmoothMat(mph, [3 3], 1);
-        bar(mph);
-        set(gca, 'xtick', [1 10 20 30 40], 'xticklabel', {'-2\pi', '-\pi', '0', '\pi', '2\pi'});
+ else
+       
+            alpha=0.05;
+                [x,y]=find(isnan(mphStat));
+            mphStat(x,:)=[];
+            %[clusters,p]=permutest(mphStat',mphC',false,alpha,1000,false);
+            [clusters,p]=permutest(mphStat',ones(size(mphStat))',false,alpha,1000,false);
+            clusters
+            p
+
+
+            sp=cell2mat(clusters(p<alpha));
+
+            data=mean(mphStat);
+            pd=makedist('uniform','lower',0,'upper',max(data));
+                       
+            [h,p]=kstest(data,'cdf',[data', cdf(pd,data')]);
+            fprintf('ks test=%f\n',p);
+            [h,p]=chi2gof(data,'Expected',ones(size(data)));
+            fprintf('chi2gof=%f\n',p);
+       
+        
+        zShift = size(mphV, 2)/2;
+    
+        mph = repeatMph(mphV,0);
+        mph = nanmean(mph);
+        
+       bar(mph);
+        
+        hold on;
+        for z = sp
+            plot([z + zShift], mph([z + zShift]), '*');
+        end
+
+        endMph = size(mph, 2);
+        t = 1:endMph;
+        set(gca, 'xtick', [1 endMph / 4 endMph / 4 * 2 endMph / 4 * 3 endMph], 'xticklabel', {'-2\pi', '-\pi', '0', '\pi', '2\pi'});
+
     end
 
-    return;
+    
+end
+
+%%%%%%%%%%%%%%
+function mph = repeatMph(mph, offset, grid, sd)
+
+    hf = size(mph, 2) / 2;
+    mph = [mph mph mph];
+    
+    if nargin > 2
+        mph = SmoothMat(mph, grid, sd);
+    end
+    
+    mph = mph(1:end-offset, hf + 1:end -hf);%cut offset
 
 end
--- PD/plotMTS.m
+++ PD/plotMTS.m
@@ -1,155 +1,210 @@
-function ind=plotMTS(Cs,phis,fs,pyr,int,confCs,elecNum,N)
-%sm=floor(10/1);
-sm=1;
-%xlabel=[5 10 15 20 25 30 35 40];
-xlabel=[ 5 10 15 20 25 30 35 40 45:5:130];
-cellNum=[];
-if nargin>6
-  for i=1:length(N)
-    cellNum=[cellNum; find(elecNum==N(i))];
-  end
-  cellNum=sort(cellNum);
-  pyr=intersect(pyr,cellNum);
-  int=intersect(int,cellNum);
-end
+function [thetaIdx,peakCoh,peakFreq] = plotMTS(Cs, phis, fs, pyr, int, confCs, Xlabel, topFreq, div, elecNum, N)
+    %sm=floor(10/1);
+    sm = 1;
+    %xlabel=[5 10 15 20 25 30 35 40];
+    %xlabel = [5 10 15 20 25 30 35 40 45:5:130];
+    cellNum = [];
 
-subplot(2,2,1);
-indPyr=coreMTS(Cs,fs,pyr,sm,xlabel,confCs);
-title('pyramidal cell');
+    if nargin > 9
+        hoge
 
-subplot(2,2,2);
-indInt=coreMTS(Cs,fs,int,sm,xlabel,confCs);
-title('interneuron');
+        for i = 1:length(N)
+            cellNum = [cellNum; find(elecNum == N(i))];
+        end
 
-
-subplot(2,2,3);
-coreMTSphi(phis,fs,pyr,sm,xlabel,indPyr);
-title('pyramidal cell');
-
-subplot(2,2,4);
-coreMTSphi(phis,fs,int,sm,xlabel,indInt);
-title('interneuron');
-
-return;
-%%%%%%%%%%%%%%%
-function indPyr=coreMTS(Cs,fs,celltype,sm,xlabel,confCs)
-
-loop=length(celltype);
-Coh=[];
-loop2=0;
-indPyr=[];
-for i=1:loop
-  Cohtmp=Cs{celltype(i)};
-  indPyr{i}=find(Cohtmp<confCs{celltype(i)});
-  Cohtmp(indPyr{i})=NaN;
-  Coh=[Coh Cohtmp];
-  loop2=loop2+1;
-end
-
-
-%Coh(1:200,:)=0;
-
-%Coh=Coh(1:4000,:);
-
-
-[M,ind]=max(Coh);
-nCoh=Coh./repmat(M,size(Coh,1),1);
-[~,ind2]=sort(ind,'descend');
-
-imagesc(nCoh(:,ind2)');
-colormap(jet);
-hold on;
-
-tmp=nanmean(Coh,2);
-
-%out=tmp./0.3;
-
-%out=tmp./max(tmp).*loop2;
-%out=tmp./max(tmp);
-%plot(tmp./max(tmp).*loop2,'k');
-%out=smooth(out,sm);
-
-sx=fs{1};
-xl=[];
-%xlabel=0:10:130;
-%xlabel=[5 10 15 20 25 30 35 40];
-for i=xlabel
-  [~,ind]=min(abs(sx-i));
-  xl=[xl ind];
-end
-out=tmp;
-minOut=min(out(xl(1):end));
-out=out-minOut;
-out=out./max(out);
-out=out.*loop;
-
-%out=tmp.*length(celltype);
-plot(out,'k');
-
-
-axis([xl(1) xl(end) 1 size(nCoh,2)]);
-
-set(gca,'xtick',xl,'xticklabel',xlabel);
-set(gca,'ydir','normal');
-return;
-%%%%%%%%%%%%%%%
-function coreMTSphi(phis,fs,celltype,sm,xlabel,indPyr)
-loop=length(celltype);
-Phis=[];
-loop2=0;
-hold on;
-
-for i=1:loop
-  tmp=phis{celltype(i)};
-  tmp(indPyr{i})=NaN;
+        cellNum = sort(cellNum);
+        pyr = intersect(pyr, cellNum);
+        int = intersect(int, cellNum);
+    end
+%pyramidal cell
+    subplot(5, 2, 1);
+    [out, xl, indPyr, thetaIdx{1},spN,peakCoh{1},peakFreq{1},Rc{1}] = coreMTS(Cs, fs, pyr, sm, Xlabel, confCs, topFreq, div);
+    title('pyramidal cell');
+    fprintf('pyramidal cell: theta index=%f\n', nanmean(thetaIdx{1}));
+    subplot(5, 2, 3);
+    plot(out,'k');
+    hold on;
+    spN(spN>length(out))=[];
+    plot(spN,out(spN),'r');
+    axis([xl(1) xl(end) 0 max(out(:))])
+    set(gca, 'xtick', xl, 'xticklabel', Xlabel);
+    set(gca, 'ydir', 'normal');
+    
   
-  Phis=[Phis tmp];
-  loop2=loop2+1;
+
+    %interneuron
+    subplot(5, 2, 2);
+    [out, ~, indInt, thetaIdx{2},spP,peakCoh{2},peakFreq{2},Rc{2}] = coreMTS(Cs, fs, int, sm, Xlabel, confCs, topFreq, div);
+    title('interneuron');
+    fprintf('interneuron: theta index=%f\n', nanmean(thetaIdx{2}));
+    spP(spP>length(out))=[];
+    subplot(5, 2, 4);
+    plot(out,'k');
+    hold on;
+    plot(spP,out(spP),'r')
+    axis([xl(1) xl(end) 0 max(out(:))])
+    set(gca, 'xtick', xl, 'xticklabel', Xlabel);
+    set(gca, 'ydir', 'normal');
+   
+    subplot(5, 2, 5);
+    coreMTSphi(phis, fs, pyr, sm, Xlabel, indPyr, div,xl,spN);
+    title('pyramidal cell');
+
+    subplot(5, 2, 6);
+    coreMTSphi(phis, fs, int, sm, Xlabel, indInt, div,xl,spP);
+    title('interneuron');
+
+    subplot(5,2,7);
+    plot(Rc{1}{1},Rc{1}{2},'.');
+    hold on;
+    xlabel('Time correlation');
+    ylabel('Frequency correlation');
+    plot([-1 1],[0 0],[0 0],[-1 1],'-.');
+    axis([-1 1 -1 1]);
+    p=signrank(Rc{1}{1},Rc{1}{2});
+    fprintf('Pyramidal cell signed rank=%f\n',p);
+
+    subplot(5,2,9);
+    plot((cell2mat(Rc{1})'));
+    set(gca,'xtick',[1 2],'xticklabel',{'Time','Freq'});
+    ylabel('Rank correlation');
+    axis([0.5 2.5 -1 1]);
+    
+    subplot(5,2,8);
+    plot(Rc{2}{1},Rc{2}{2},'.');
+    hold on;
+    xlabel('Time correlation');
+    ylabel('Frequency correlation');
+    plot([-1 1],[0 0],[0 0],[-1 1],'-.');
+    axis([-1 1 -1 1]);
+    p=signrank(Rc{2}{1},Rc{2}{2});
+    fprintf('Interneuron signed rank%f\n',p);
+
+    subplot(5,2,10);
+    plot((cell2mat(Rc{2})'));
+    set(gca,'xtick',[1 2],'xticklabel',{'Time','Freq'});
+    ylabel('Rank correlation');
+    axis([0.5 2.5 -1 1]);
+    set(gcf,'position',[100 100 400 800]);
 end
 
-%Phis(1:200,:)=0;
-
-%tmp=(nanmean(Phis,2)./pi)*180;
-tmp=nanmedian(Phis,2);
-%tmp=Phis./pi.*180;
-
-if ~isempty(tmp)
+%%%%%%%%%%%%%%%
+function [out, xl, indPyr, thetaIdx, sp, peakCoh, peakFreq,Rc] = coreMTS(Cs, fs, celltype, sm, xlabel, confCs, topFreq, div)
   
-%  [~,ind]=max(Phis);
-%  [~,ind2]=sort(ind,'descend');
-%  imagesc(Phis(:,ind2)');
-%colorbar;
-%  hold on;
-%  plot(fs{1},tmp,'k');
+    loop = length(celltype);
+    Coh = [];
+    Ctrl = [];
+    loop2 = 0;
+    indPyr = [];
+    CohAS=[];
+    CohDS=[];
+    for i = 1:loop
+        Cohtmp = Cs{celltype(i)};
+        
+        %CtrlTmp = squeeze(Cohtmp(:, :, div + 3));
+        CtrlTmp = squeeze(Cohtmp(:, :, 6));
+        CohtmpAS = squeeze(Cohtmp(:, :, 1));
+        CohtmpDS = squeeze(Cohtmp(:, :, 2));
+        Cohtmp=squeeze(Cohtmp(:,:,div));
+  
+        Coh = [Coh Cohtmp];
+        Ctrl = [Ctrl CtrlTmp];
+        %rank correlation
+        CohAS=[CohAS CohtmpAS];
+        CohDS=[CohDS CohtmpDS];
+    end
+  
+    sx = fs{1};
+    xl = [];
 
-sm=Phis;
-for i=1:size(Phis,2)
-%  sm(:,i)=smooth(Phis(:,i),100);
-%  plot(fs{i},smooth(Phis(:,i),100));
+    for i = xlabel
+        [~, ind] = min(abs(sx - i));
+        xl = [xl ind - 1];
+    end
+    endA = find(xlabel == topFreq);
+  
+    [peakCoh,peakFreq]=max(Coh(1:xl(endA),:));    
+
+    peakFreq=sx(peakFreq);
+   
+    %rank correlation
+    CohAS=CohAS(1:xl(endA),:);
+    CohDS=CohDS(1:xl(endA),:);
+ 
+    Frc=diag(corr(CohAS,CohDS,'type','pearson'));%frequency
+    Trc=diag(corr(CohAS,CohDS(end:-1:1,:),'type','spearman'));%time
+    
+    Rc{1}=Trc;
+    Rc{2}=Frc;
+
+    [M, ind] = max(Coh(1:xl(endA),:));
+    nCoh = Coh ./ repmat(M, size(Coh, 1), 1);
+    [~, ind2] = sort(ind, 'ascend');
+    tmp = nCoh(1:xl(endA), ind2);
+    imagesc(tmp');
+    colormap(jet);
+    hold on;
+  
+    tmp = nanmean(Coh(1:xl(endA),:), 2);
+   
+    out = tmp;
+    startT = find(xlabel == 2);
+    endT = find(xlabel == 12);
+    thetaIdx = xl(startT):xl(endT);
+
+    startA = find(xlabel == 14);
+    allIdx = xl(startA):xl(endA);
+    thetaIdx = nanmean(Coh(thetaIdx, :), 1) ./ nanmean(Coh(allIdx, :), 1);
+    
+    if ~isempty(nCoh)
+        axis([xl(1) xl(end) 1 size(nCoh, 2)]);
+    end
+
+    set(gca, 'xtick', xl, 'xticklabel', xlabel);
+    set(gca, 'ydir', 'normal');
+    
+    %permutation test
+    alpha = 0.05;
+  
+    Coh=Coh(allIdx,:);
+    Ctrl=Ctrl(allIdx,:);
+    [clusters, p] = permutest(Coh, Ctrl, false, alpha, 1000, false);
+   
+    sp = cell2mat(clusters(p < alpha));
+
 end
-plot(fs{i},nanmean(sm,2));
-%  plot(fs{1},smooth(tmp,sm),'k');
-%  axis([0 40 -180 180]);
+
+%%%%%%%%%%%%%%%
+function coreMTSphi(phis, fs, celltype, sm, xlabel, indPyr, div,xl,sp)
+    loop = length(celltype);
+    Phis = [];
+    loop2 = 0;
+
+    for i = 1:loop
+        tmp = phis{celltype(i)};
+
+        tmp = squeeze(tmp(:, :, div));
+
+        Phis = [Phis tmp];
+        loop2 = loop2 + 1;
+    end
+    %[~,Neu]=find(isnan(Phis));
+    %Phis(:,Neu)=[];
+    tmp = unwrap(circ_mean(Phis'));
+
+    sx = fs{1};
+   
+    hold on;
+    plot(tmp,'k');
+
+    plot(sp, tmp(sp),'r');
+    plot([xl(1) xl(end)], [-pi  -pi ], 'k');
+    plot([xl(1) xl(end)], [0 0], 'k');
+    plot([xl(1) xl(end)], [pi pi ], 'k');
+    axis([xl(1) xl(end) -2*pi 2*pi]);
+    set(gca, 'xtick', xl, 'xticklabel', xlabel,'ytick',[-pi 0 pi],'yticklabel',{'-\pi' '0' '\pi'});
+    set(gca, 'ydir', 'normal');
+
+    %save test.mat Phis
 
 end
-
-
-%xlabel=[ 5 10 15 20 25 30 35 40];
-
-axis([xlabel(1) xlabel(end) -pi pi]);
-set(gca,'xtick',xlabel,'xticklabel',xlabel);
-set(gca,'ydir','normal');
-
-%save test.mat Phis
-
-return;
-sx=fs{1};
-xl=[];
-xlabel=0:10:130;
-for i=xlabel
-  [~,ind]=min(abs(sx-i));
-  xl=[xl ind];
-end
-set(gca,'xtick',xl,'xticklabel',xlabel);
-set(gca,'ydir','normal');
-return;
